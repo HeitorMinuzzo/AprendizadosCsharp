4029f36dcda0cfb4ec0b621704af4563c8c522b5
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-12 19:04:06 -0300

    Add files via upload

---
+++ MédiaDe3Números/projetoc#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoc_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3;
+            double media;
+            Console.WriteLine("Insira o numero 1");
+            num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o numero 2");
+            num2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o numero 3");
+            num3 = Double.Parse(Console.ReadLine());
+            media = (num1 + num2 + num3) / 3;
+            Console.WriteLine("A média é: " + media);
+            Console.ReadKey();
+
+        }
+    }
+}
