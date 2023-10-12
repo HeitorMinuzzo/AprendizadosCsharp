866252c48da2ce514e97d427321410c23a62ad09
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-12 19:21:48 -0300

    Add files via upload

---
+++ TestandoFor2/TestandoFor2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestandoFor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, final, num;
+            Console.WriteLine("insira o numero inicial: ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("insira o numero final: ");
+            final = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sequência: ");
+            for (i = num; i <= final; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
