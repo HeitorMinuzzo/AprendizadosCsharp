3ce84637af44093e320b5d679de717c976b725f8
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-12 19:19:01 -0300

    Add files via upload

---
+++ TestandoFor/TestandoFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestandoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+            Console.WriteLine("insira o numero inicial: ");
+            num = int.Parse(Console.ReadLine());
+            for (i = num; i <= 500; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
